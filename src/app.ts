5d0b4f9c7f983fc2335eb7c6791c4e58ddf82d3a
Ashikur Rahman <ashikmoinkhan@gmail.com>
2023-11-21 17:16:25 +0600

    Project setup Done

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import cors from "cors";
 import express, { Application, Request, Response } from "express";
 const app: Application = express();
 
-// parsers
 app.use(express.json());
 app.use(cors());
 
